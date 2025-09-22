03de6848cc9d5ad137f236e6a6664be4e3f8dc66
Swami Romão <swamiromao12@gmail.com>
2025-09-22 11:15:43 -0300

    add new

--- src/foundations/logic/bSequential_logic/Example.java
+++ src/foundations/logic/bSequential_logic/Example.java
@@ -4,6 +4,12 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("Hello World");
         // Always moving downward sequentially
-        System.out.println("let's go");
+        System.out.println("let's go pt.1");
+        System.out.println("let's go pt.2");
+        System.out.println("let's go pt.3");
+        System.out.println("let's go pt.4");
+        System.out.println("let's go pt.5");
+        System.out.println("let's go pt.6");
+        System.out.println("let's go pt.7");
     }
 }
---
+++ src/foundations/logic/cVariables_primitiveTypes/primitiveTypes.java
@@ -0,0 +1,19 @@
+package foundations.logic.cVariables_primitiveTypes;
+
+public class primitiveTypes {
+    public static void main(String[] args) {
+
+        int age = 25;
+        long numberOfDogs = 3L;
+        double annualSalary = 93000.00;
+        char gender = 'M';
+        boolean employee = true;
+
+        System.out.println("\nInformation by Dev \n");
+        System.out.println("Age: " + age);
+        System.out.println("Annaul Salary: $" + annualSalary);
+        System.out.println("Gender: " + gender);
+        System.out.println("employee: " + employee);
+        System.out.println("Number of Dogs: " + numberOfDogs);
+    }
+}
