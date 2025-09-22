da102a5c32041e2e3623dddcd085d3ebdf4448a0
Swami Romão <swamiromao12@gmail.com>
2025-09-22 10:20:08 -0300

    add new

---
+++ src/foundations/logic/bSequential_logic/Example.java
@@ -0,0 +1,9 @@
+package foundations.logic.bSequential_logic;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // Always moving downward sequentially
+        System.out.println("let's go");
+    }
+}
