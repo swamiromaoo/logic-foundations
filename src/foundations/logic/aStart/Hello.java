b6d23fb8efc05e449b948da9c5dcf69dab47f71c
Swami Romão <swamiromao12@gmail.com>
2025-09-22 10:05:04 -0300

    Initial commit

---
+++ src/foundations/logic/aStart/Hello.java
@@ -0,0 +1,7 @@
+package foundations.logic.aStart;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, start here!!");
+    }
+}
---
+++ src/foundations/logic/sequential_logic/Example.java
@@ -0,0 +1,7 @@
+package foundations.logic.sequential_logic;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
