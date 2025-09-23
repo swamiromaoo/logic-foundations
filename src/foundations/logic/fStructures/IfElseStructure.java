caccf0e338570a7804b6e56c0a4270fe4d305587
Swami Romão <swamiromao12@gmail.com>
2025-09-23 11:13:13 -0300

    add new

---
+++ src/foundations/logic/fStructures/IfElseStructure.java
@@ -0,0 +1,36 @@
+package foundations.logic.fStructures;
+
+public class IfElseStructure {
+    public static void main(String[] args) {
+        int age = 38;
+//
+//        if (age >= 18) {
+//            System.out.println("You are of legal age!");
+//        }
+//        else {
+//            System.out.println("You are a minor!!");
+//        }
+
+//        boolean itsRaining = true;
+//
+//        if (!itsRaining){
+//            System.out.println("Let's go out");
+//        }
+//        else {
+//            System.out.println("Let's stay at home");
+//        }
+
+        boolean thereIsMoney = true;
+        boolean itHasACard = false;
+
+        if (thereIsMoney && itHasACard){
+            System.out.println("Let's order food and beer!");
+        } else if (thereIsMoney || itHasACard) {
+            System.out.println("Let's just order food");
+            
+        }else {
+            System.out.println("Let's not order food!");
+        }
+    }
+
+}
