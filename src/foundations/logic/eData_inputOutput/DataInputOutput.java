49f3ac2ef5b396bb425ce6a94a8394553bd0f6fc
Swami Romão <swamiromao12@gmail.com>
2025-09-23 09:24:18 -0300

    add new

---
+++ src/foundations/logic/eData_inputOutput/DataInputOutput.java
@@ -0,0 +1,33 @@
+package foundations.logic.eData_inputOutput;
+
+import java.util.Scanner;
+
+public class DataInputOutput {
+    public static void main(String[] args) {
+
+//        System.out.println("I do the print and skip the line");
+//        System.out.print("I do the print on the same line");
+//        System.out.printf("I do the formatted print");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Enter how much you receive: ");
+        double salary = scanner.nextDouble();
+
+        System.out.println("Enter if you are working yes(true) or no(false)");
+        boolean job = scanner.nextBoolean();
+
+
+        System.out.println("\nName: " + name.toUpperCase());
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Working: " + job);
+
+    }
+}
