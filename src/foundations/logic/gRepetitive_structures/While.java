3340896ff057f786eef82153067dd35fe0bdac20
Swami Romão <swamiromao12@gmail.com>
2025-09-24 09:36:57 -0300

    add new

--- src/foundations/logic/gRepetitive_structures/While.java
+++ src/foundations/logic/gRepetitive_structures/While.java
@@ -2,11 +2,19 @@ package foundations.logic.gRepetitive_structures;
 
 public class While {
     public static void main(String[] args) {
-        int Counter = 0;
+        int counter = 0;
 
-        while (Counter <= 100) {
-            System.out.println("Counter: " + Counter);
-            Counter++;
+        while (counter <= 15) {
+            System.out.println("Counter: " + counter);
+           counter++;
         }
+        System.out.println("-----------------------------------\n");
+
+        int counterTwo = 0;
+
+        do {
+            System.out.println("Counter do-while: " + counterTwo);
+            counterTwo++;
+        }while (counterTwo <= 20);
     }
 }
